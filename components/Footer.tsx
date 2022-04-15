c18eae1b2b14df22a8604127fe88d62211848441
Bastien Scanu <BastienScanu@users.noreply.github.com>
2022-04-15 18:29:50 +0200

    set-up next-i18next

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,19 +1,14 @@
+import { useTranslation } from 'next-i18next';
 import * as React from 'react'
-import Image from 'next/image'
 
-const Footer = () => (
-  <footer>
-  <a
-    href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-    target="_blank"
-    rel="noopener noreferrer"
-  >
-    Powered by{' '}
-    <span>
-      <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-    </span>
-  </a>
-</footer>
-)
+const Footer = () => {
+  const { t } = useTranslation('footer');
+
+  return (
+    <footer>
+      {t('footer')}
+    </footer>
+  )
+}
 
 export default Footer
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,24 @@
 import type { NextPage } from 'next'
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
+import { useTranslation } from 'next-i18next'
 import styles from '../styles/Home.module.css'
 import Layout from '../components/Layout'
 
+
+export async function getStaticProps({ locale }: {locale: string}) {
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, ['common', 'header', 'footer'])),
+    },
+  };
+}
+
 const Home: NextPage = () => {
+  const { t } = useTranslation('common');
   return (
     <Layout title="Bastien Scanu">
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+         {t('hello')}
         </h1>
     </Layout>
   )
