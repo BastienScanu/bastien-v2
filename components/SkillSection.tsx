67ecda3e5ebe466053c9cef3cfaf85b88d5c2493
Bastien Scanu <BastienScanu@users.noreply.github.com>
2022-04-23 14:09:44 +0200

    display logos in skills sections

--- components/SkillSection.tsx
+++ components/SkillSection.tsx
@@ -1,25 +1,37 @@
 import { useTranslation } from 'next-i18next';
 import * as React from 'react'
+import Image from 'next/image'
 import Skill from '../models/Skill'
+import styles from '../styles/SkillSection.module.scss'
 
 const SkillSection = ({skill}: {skill: Skill}) => {
   const { t } = useTranslation(skill.name)
 
   return (
     <div id={skill.name}>
-      <h2>
-        {t('title')}
-      </h2>
-      <p>
-        {t('desc')}
-      </p>
-      <ul>
-      {skill.technos.map((techno: string) => {
-        return (
-          <li key={techno}>{t(`technos.${techno}`)}</li>
-        );
-      })}
-      </ul>
+      <div className='pure-u-2-3'>
+        <h2>
+          {t('title')}
+        </h2>
+        <p>
+          {t('desc')}
+        </p>
+        <div className='pure-u-1-2'>
+          {skill.technos.map((techno: string) => {
+            return (
+              <div key={techno} className={styles.logoFrame}>
+                <Image src={`/images/technos/${techno}-color.svg`} className={styles.logo}
+                height={64} width={128} alt = {techno + " logo"} />
+              </div>
+            );
+          })}
+        </div>
+        <div className='pure-u-1-2'>
+        </div>
+      </div>
+      <div className='pure-u-1-3'>
+        
+      </div>
     </div>
   )
 }
