412c60bfb4460f44140cf3b329c449a857608d65
Bastien Scanu <bastien.scanu@protonmail.com>
2022-07-02 11:24:41 +0200

    header with responsive navbar

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,36 +1,89 @@
 import * as React from 'react'
 import Link from 'next/link'
+import Image from 'next/image'
 import { useTranslation } from 'next-i18next'
+import styles from '../styles/Header.module.scss'
+import { useEffect, useState } from 'react'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const Header = () => {
   const { t } = useTranslation('header')
+  const { i18n } = useTranslation('header')
+  const lang = i18n.language === 'fr' ? 'en' : 'fr'
+
+  const [scrollY, setScrollY] = useState(0)
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrollY(window.scrollY)
+    }
+
+    handleScroll()
+
+    window.addEventListener('scroll', handleScroll)
+    return () => {
+      window.removeEventListener('scroll', handleScroll)
+    }
+  }, [])
+
+  const [isMenuActive, setMenuActive] = useState(false)
+
+  const handleMenu = () => {
+    setMenuActive(!isMenuActive)
+  }
 
   return (
-    <header className="header">
-      <nav className="pure-menu pure-menu-horizontal pure-menu-fixed">
-        <ul className="pure-menu-list">
-          <li className="pure-menu-item">
-            <Link href="#home">
-              <a className="pure-menu-link">{t('home')}</a>
-            </Link>
-          </li>
-          <li className="pure-menu-item">
-            <Link href="#about">
-              <a className="pure-menu-link">{t('about')}</a>
-            </Link>
-          </li>
-          <li className="pure-menu-item">
-            <Link href="#skills">
-              <a className="pure-menu-link">{t('skills')}</a>
-            </Link>
-          </li>
-          <li className="pure-menu-item">
-            <Link href="#experience">
-              <a className="pure-menu-link">{t('experience')}</a>
-            </Link>
-          </li>
-        </ul>
-      </nav>
+    <header
+      className={styles.header + (scrollY > 50 ? '' : ' ' + styles.transparent)}
+    >
+      <div className={styles.content}>
+        <div className={styles.logo}>
+          <Link href="/">
+            <a>
+              <Image
+                src={`/images/layout/logo-texte.svg`}
+                height="70px"
+                width="120px"
+                alt={'Logo Bastien Scanu'}
+              />
+            </a>
+          </Link>
+        </div>
+        <nav className={isMenuActive ? styles.active : ''}>
+          <ul>
+            <li onClick={handleMenu}>
+              <Link href="#home">
+                <a>{t('home')}</a>
+              </Link>
+            </li>
+            <li onClick={handleMenu}>
+              <Link href="#about">
+                <a>{t('about')}</a>
+              </Link>
+            </li>
+            <li onClick={handleMenu}>
+              <Link href="#skills">
+                <a>{t('skills')}</a>
+              </Link>
+            </li>
+            <li onClick={handleMenu}>
+              <Link href="#experience">
+                <a>{t('experience')}</a>
+              </Link>
+            </li>
+          </ul>
+        </nav>
+        <div className={styles.rightMenu}>
+          <Link href="" locale={lang}>
+            <a className={styles.lang}>{lang.toUpperCase()}</a>
+          </Link>
+          <FontAwesomeIcon
+            icon={faBars}
+            className={styles.hamburger}
+            onClick={handleMenu}
+          />
+        </div>
+      </div>
     </header>
   )
 }
