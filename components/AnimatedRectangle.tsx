fb5d8d524be35abe77a04909b1084e8ed6a67351
Bastien Scanu <bastien.scanu@protonmail.com>
2024-01-14 15:26:43 +0100

    Animated logo

---
+++ components/AnimatedRectangle.tsx
@@ -0,0 +1,56 @@
+import * as React from 'react'
+
+const AnimatedRectangle = ({
+  start,
+  stop,
+  width,
+  x,
+  y,
+  color,
+}: {
+  start: number
+  stop: number
+  width: number
+  x: number
+  y: number
+  color: string
+}) => {
+  const coeff = 0.7
+  const height = 42 * coeff
+  width = width * coeff
+  return (
+    <React.Fragment>
+      <style>
+        {`
+      #rectangle_${x}_${y} {
+        animation: rectangle_${x}_${y}_anim 4000ms linear 1 normal forwards;
+      }
+      @keyframes rectangle_${x}_${y}_anim {
+        0% {
+            width: 0px;
+          }
+          ${start}% {
+            width: 0px;
+          }
+          ${stop}% {
+            width: ${width}px;
+          }
+          100% {
+            width: ${width}px;
+          }
+      }`}
+      </style>
+      <rect
+        id={`rectangle_${x}_${y}`}
+        width="0"
+        height={84 * coeff}
+        rx={42 * coeff}
+        ry={42 * coeff}
+        transform={`translate(${x * coeff} ${y * coeff})`}
+        fill={color}
+      />
+    </React.Fragment>
+  )
+}
+
+export default AnimatedRectangle
--- components/Home.tsx
+++ components/Home.tsx
@@ -1,59 +1,25 @@
 import { useTranslation } from 'next-i18next'
 import * as React from 'react'
-import Image from 'next/image'
 import styles from '../styles/Home.module.scss'
-import ParallaxElement from './ParallaxElement'
+import AnimatedLogo from './AnimatedLogo'
 
 const Home = () => {
   const { t } = useTranslation('home')
   return (
     <section id="home" className="pure-g">
       <div className="pure-u-1-1 pure-u-md-1-2">
-        <ParallaxElement speed={-15}>
-          <p className={styles.title}>
-            {t('hi')}
-            <br />
-            {t('iam')} <span className="green">Bastien,</span>
-            <br />
-            {t('job1')}
-            <br />
-            {t('job2')}
-          </p>
-        </ParallaxElement>
+        <AnimatedLogo />
       </div>
       <div className="pure-u-1-1 pure-u-md-1-2">
-        <ParallaxElement className={styles.smartphone} speed={-5}>
-          <Image
-            src={`/images/layout/smartphone.svg`}
-            height={150}
-            width={150}
-            alt="big screen"
-          />
-        </ParallaxElement>
-        <ParallaxElement className={styles.bigScreen} speed={-5}>
-          <Image
-            src={`/images/layout/big-screen.svg`}
-            height={600}
-            width={600}
-            alt="big screen"
-          />
-        </ParallaxElement>
-        <ParallaxElement className={styles.laptop} speed={0}>
-          <Image
-            src={`/images/layout/laptop.svg`}
-            height={300}
-            width={300}
-            alt="laptop"
-          />
-        </ParallaxElement>
-        <ParallaxElement className={styles.mouse} speed={15}>
-          <Image
-            src={`/images/layout/mouse.svg`}
-            height={64}
-            width={64}
-            alt="mouse"
-          />
-        </ParallaxElement>
+        <p className={styles.title}>
+          {t('hi')}
+          <br />
+          {t('iam')} <span className="green">Bastien,</span>
+          <br />
+          {t('job1')}
+          <br />
+          {t('job2')}
+        </p>
       </div>
     </section>
   )
