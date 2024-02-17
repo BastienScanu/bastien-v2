93c6effb7498829aaea92a740fcaf8ecddd29b48
Bastien Scanu <bastien.scanu@protonmail.com>
2024-02-17 05:56:01 +0100

    fix fonts and css imports

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,23 +1,26 @@
-import '../styles/globals.scss';
-import Hero from '../components/Hero';
-import About from '../components/About';
-import Skills from '../components/Skills';
-import ExperienceSection from '../components/ExperienceSection';
-import { Fragment } from 'react';
+import 'purecss/build/base-min.css'
+import 'purecss/build/grids-min.css'
+import 'purecss/build/grids-responsive-min.css'
+import '../styles/globals.scss'
+import Hero from '../components/Hero'
+import About from '../components/About'
+import Skills from '../components/Skills'
+import ExperienceSection from '../components/ExperienceSection'
+import { Fragment } from 'react'
 
 export interface Props {
   params: {
-    locale: string;
+    locale: string
   }
 }
 
 export default async function Home({ params: { locale } }: Props) {
   return (
     <Fragment>
-      <Hero locale={locale}/>
-      <About locale={locale}/>
-      <Skills locale={locale}/>
-      <ExperienceSection locale={locale}/>
+      <Hero locale={locale} />
+      <About locale={locale} />
+      <Skills locale={locale} />
+      <ExperienceSection locale={locale} />
     </Fragment>
-  );
+  )
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,25 +1,39 @@
-import type { Metadata } from "next";
-import i18nConfig from "../../i18nConfig";
+import type { Metadata } from 'next'
+import { ReactNode } from 'react'
+import i18nConfig from '../../i18nConfig'
+import { Fira_Sans } from 'next/font/google'
+import { Plus_Jakarta_Sans } from 'next/font/google'
+
+const fira = Fira_Sans({
+  subsets: ['latin'],
+  weight: ['400', '500', '600'],
+  variable: '--font-fira',
+})
+
+const jakarta = Plus_Jakarta_Sans({
+  subsets: ['latin'],
+  variable: '--font-jakarta',
+})
 
 export const metadata: Metadata = {
-  title: "Bastien Scanu",
-  description: "My personal website",
-};
+  title: 'Bastien Scanu',
+  description: 'My personal website',
+}
 
 export function generateStaticParams() {
-  return i18nConfig.locales.map(locale => ({ locale }));
+  return i18nConfig.locales.map((locale) => ({ locale }))
 }
 
 export default async function RootLayout({
   children,
-  params: { locale }
+  params: { locale },
 }: Readonly<{
-  children: React.ReactNode;
-  params: { locale: string}
+  children: ReactNode
+  params: { locale: string }
 }>) {
   return (
-    <html lang={locale}>
+    <html lang={locale} className={`${jakarta.variable} ${fira.variable}`}>
       <body>{children}</body>
     </html>
-  );
+  )
 }
